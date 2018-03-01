822f35b6764b189df6e5c5aedd5050c0910d9cd5
Sarah Strange <sarah.strange@availity.com>
2018-03-01 10:33:25 -0500

    adjusting scope

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,10 +1,19 @@
 app.controller('MainController', ['$scope', function($scope) { 
     $scope.title = 'This Month\'s Bestsellers'; 
     $scope.promo = 'The most popular books this month.';
-    $scope.product = { 
-        name: 'The Book of Trees', 
-        price: 19,
-        pubdate: new Date('2009', '02', '24')
-      }
+    $scope.products = [ 
+        { 
+          name: 'The Book of Trees', 
+          price: 19, 
+          pubdate: new Date('2014', '03', '08'), 
+          cover: 'img/the-book-of-trees.jpg' 
+        }, 
+        { 
+          name: 'Program or be Programmed', 
+          price: 8, 
+          pubdate: new Date('2013', '08', '01'), 
+          cover: 'img/program-or-be-programmed.jpg' 
+        } 
+      ]
   }]);
   
