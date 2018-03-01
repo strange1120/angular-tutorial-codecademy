2a69b678b44e5079617066b46c1ecc25385670fb
Sarah Strange <sarah.strange@availity.com>
2018-03-01 12:28:49 -0500

    added like and dislike functionality

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,31 +7,38 @@ app.controller('MainController', ['$scope', function($scope) {
           price: 19, 
           pubdate: new Date('2014', '03', '08'), 
           cover: 'img/the-book-of-trees.jpg',
-          likes: 0
+          likes: 0,
+          dislikes: 0
         }, 
         { 
           name: 'Program or be Programmed', 
           price: 8, 
           pubdate: new Date('2013', '08', '01'), 
           cover: 'img/program-or-be-programmed.jpg',
-          likes: 0 
+          likes: 0,
+          dislikes: 0
         } ,
         {
           name:'Harry Potter and the Order of the Phoenix',
           price: 24,
           pubdate: new Date('2007', '07', '04'),
           cover: 'img/program-or-be-programmed.jpg',
-          likes: 0
+          likes: 0,
+          dislikes: 0
         },
         {
           name:'The Hunger Games',
           price: 12,
           pubdate: new Date('2009', '08', '23'),
           cover: 'img/program-or-be-programmed.jpg',
-          likes: 0
+          likes: 0,
+          dislikes: 0
         }
       ];
       $scope.plusOne = function(index) {
+          $scope.products[index].likes += 1;
+      };
+      $scope.minusOne = function(index) {
           $scope.products[index].likes += 1;
       };
   }]);
