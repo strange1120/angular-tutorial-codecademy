764446e1b7eb5b6f90e3e61e62262dd4e053ea3d
Sarah Strange <sarah.strange@availity.com>
2018-03-01 10:21:25 -0500

    currency and pubdate

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -3,7 +3,8 @@ app.controller('MainController', ['$scope', function($scope) {
     $scope.promo = 'The most popular books this month.';
     $scope.product = { 
         name: 'The Book of Trees', 
-        price: 19 
+        price: 19,
+        pubdate: new Date('2009', '02', '24')
       }
   }]);
   
